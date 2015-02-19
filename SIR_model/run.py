ab1f192d91d4ec234c5901b9af9e3fadcde2dd9a
syaksic <seanyabe@gmail.com>
2015-02-18 23:45:06 -0300

    commit 2015-02-18-23-44-55

--- SIR_model/run.py
+++ SIR_model/run.py
@@ -10,11 +10,23 @@ def Calculate():
 		set_graph(graph)
 	inputs=SIR['inputs']
 	if not(inputs['status']=='READY'):
-		set_inputs(inputs,0.2,0.4,0.1,0.3,1.6,0.9)
-	
-		#Calcular Grafo
-	print('Falta hacer esto')
-	print(SIR)
+		#tasa de contacto: 1 usuario cada 1 segundo
+		mean_contact_rate=1.0/1.0
+		#tasa de olvido: 1 olvido cada 1 dia 86400 segundos
+		mean_forgetting_rate=1.0/3000.0
+		#tasa de recuerdo: 1 recuerdo cada 10 dias 864000 segundos
+		mean_remember_rate=1.0/8640000.0
+		#probabilidade de ostigarse de un rumor 0.1
+		stifling_prob=0.9
+		#probabilidade de rechazar/aceptar un rumor 0.9/0.1
+		reffusing_prob=0.9
+		#probabilidade de no-recapacitar/recapacitar sobre un rumor 0.99/0.01
+		unrethinking_prob=0.9
+		set_inputs(inputs,
+			stifling_prob,reffusing_prob,stifling_prob,
+			mean_contact_rate,mean_forgetting_rate,mean_remember_rate)
+	execute(SIR)
 	return 
 
+#execute({})
 Calculate()
